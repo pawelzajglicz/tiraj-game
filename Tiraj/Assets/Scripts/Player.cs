ef073f2fbad2271c4845ed083909fd7fb92c3e0b
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2019-10-30 18:51:11 +0100

    Player moving
    Making Player moving

--- Tiraj/Assets/Scripts/Player.cs
+++ Tiraj/Assets/Scripts/Player.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float speed = 500;
+    public float jumpForce = 325;
+    private bool isGrounded;
+
+    private float xDisplacement;
+
+    private Rigidbody2D rigidBody;
+
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
+        isGrounded = false;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        xDisplacement = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        rigidBody.velocity = new Vector2(xDisplacement, rigidBody.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rigidBody.AddForce(new Vector2(0, jumpForce));
+            isGrounded = false;
+        }
+
+        if (rigidBody.velocity.x < 0)
+        {
+            transform.rotation = Quaternion.AngleAxis(180, Vector3.up);
+        }
+        else if (rigidBody.velocity.x > 0)
+        {
+            transform.rotation = Quaternion.AngleAxis(0, Vector3.up);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
     }
 }
