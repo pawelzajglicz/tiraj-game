455896195426b7526f7f45e0d280a694d174e893
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2019-11-03 00:03:26 +0100

    Adding Portal script
    Adding Portal script

---
+++ Tiraj/Assets/Scripts/Portal.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            Player player = collider.gameObject.GetComponent<Player>();
+            player.Successed();
+        }
+    }
+}
