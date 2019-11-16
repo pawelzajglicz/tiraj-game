af3dce459499d34e7eccdbad277b05b5916922ab
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2019-11-16 15:53:40 +0100

    Comet
    Adding falling comets

---
+++ Tiraj/Assets/Scripts/CometSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CometSpawner : MonoBehaviour
+{
+    public Comet comet;
+
+    public float frequency = 1;
+    private float lastCometTime;
+    private float direction = -1;
+
+    public Vector3 position;
+    
+
+    void Start()
+    {
+        lastCometTime = 0;
+        position = transform.position;
+    }
+    
+    void Update()
+    {
+        lastCometTime += Time.deltaTime;
+
+        if (lastCometTime >= frequency)
+        {
+            Instantiate(comet, position, Quaternion.identity);
+            lastCometTime = 0;
+        }
+        
+    }
+
+}
+
